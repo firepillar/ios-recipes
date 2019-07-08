67f3845f81b47479ae61dad173f281c394d3733a
Kat Milton <mltnkat@gmail.com>
2019-07-08 12:41:10 -0700

    update table view.

--- Recipes/Recipes/View Controllers/RecipesTableViewController.swift
+++ Recipes/Recipes/View Controllers/RecipesTableViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class RecipesTableViewController: UITableViewController {
 
+    
+    var recipes: [Recipe] = [] {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -29,18 +36,20 @@ class RecipesTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return recipes.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RecipeCell", for: indexPath)
 
         // Configure the cell...
+        let recipe = recipes[indexPath.row]
+        cell.textLabel?.text = recipe.name
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
@@ -77,14 +86,22 @@ class RecipesTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        if segue.identifier == "ShowRecipe" {
+            guard let recipeDetailVC = segue.destination as? RecipeDetailViewController, let index = tableView.indexPathForSelectedRow else { return }
+            let recipe = recipes[index.row]
+            recipeDetailVC.recipe = recipe
+            
+        }
+        
     }
-    */
+    
 
 }
