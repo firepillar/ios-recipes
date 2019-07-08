cc919cc0b13e969780b045eb8244e39af4c6874e
Kat Milton <mltnkat@gmail.com>
2019-07-08 13:21:27 -0700

    MVP upload.

--- Recipes/Recipes/View Controllers/MainViewController.swift
+++ Recipes/Recipes/View Controllers/MainViewController.swift
@@ -11,26 +11,76 @@ import UIKit
 class MainViewController: UIViewController {
     
     @IBOutlet var searchTextField: UITextField!
-
+    
+    let networkClient = RecipesNetworkClient()
+    
+    var allRecipes: [Recipe] = [] {
+        didSet {
+            filterRecipes()
+        }
+    }
+    
+    var recipesTableViewController: RecipesTableViewController? {
+        didSet {
+            self.recipesTableViewController?.recipes = filteredRecipes
+        }
+    }
+    
+    var filteredRecipes: [Recipe] = [] {
+        didSet {
+            recipesTableViewController?.recipes = self.filteredRecipes
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        networkClient.fetchRecipes { allRecipes, error in
+            
+            if let error = error {
+                let errorText: () = NSLog("Error fetching recipes: \(error)")
+                return errorText
+            }
+            
+            DispatchQueue.main.async {
+                self.allRecipes = allRecipes ?? []
+            }
+            
+            
+            
+        }
         // Do any additional setup after loading the view.
     }
     
 
     @IBAction func searchTextEdited(_ sender: UITextField) {
+        resignFirstResponder()
+        filterRecipes()
+    }
+    
+    
+    func filterRecipes() {
+        guard let searchTerm = searchTextField.text else { return }
+        
+        if searchTerm == "" {
+            filteredRecipes = allRecipes
+        } else {
+            filteredRecipes = allRecipes.filter { $0.name.contains(searchTerm) || $0.instructions.contains(searchTerm) }
+        }
         
     }
     
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        if segue.identifier == "RecipeTableSegue" {
+            recipesTableViewController = (segue.destination as! RecipesTableViewController)
+        }
     }
-    */
+    
 
 }
--- Recipes/Recipes/View Controllers/RecipesTableViewController.swift
+++ Recipes/Recipes/View Controllers/RecipesTableViewController.swift
@@ -19,20 +19,11 @@ class RecipesTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+   
     }
 
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
@@ -51,40 +42,15 @@ class RecipesTableViewController: UITableViewController {
     }
     
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    private func recipeFor(indexPath: IndexPath) -> Recipe {
+        if indexPath.section == 0 {
+            return recipes[indexPath.row]
+        } else {
+            return recipes[indexPath.row]
+        }
+        
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     
     // MARK: - Navigation
@@ -95,9 +61,9 @@ class RecipesTableViewController: UITableViewController {
         // Pass the selected object to the new view controller.
         
         if segue.identifier == "ShowRecipe" {
-            guard let recipeDetailVC = segue.destination as? RecipeDetailViewController, let index = tableView.indexPathForSelectedRow else { return }
-            let recipe = recipes[index.row]
-            recipeDetailVC.recipe = recipe
+            guard let recipeDetailVC = segue.destination as? RecipeDetailViewController else { return }
+            guard let index = tableView.indexPathForSelectedRow else { return }
+            recipeDetailVC.recipe = recipeFor(indexPath: index)
             
         }
         
